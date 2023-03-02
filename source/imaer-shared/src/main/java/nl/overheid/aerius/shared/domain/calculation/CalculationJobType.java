fe3772828e9fc5dc05bfa93001ac7070a68eda6b
Gijsvanhorn <82368632+Gijsvanhorn@users.noreply.github.com>
2023-03-02 15:33:42 +0100

    AER-1379 add single scenario type (#186)

--- source/imaer-shared/src/main/java/nl/overheid/aerius/shared/domain/calculation/CalculationJobType.java
+++ source/imaer-shared/src/main/java/nl/overheid/aerius/shared/domain/calculation/CalculationJobType.java
@@ -37,8 +37,10 @@ public enum CalculationJobType {
    * Optional: 1 Reference
    * NO other scenario types
    */
-  PROCESS_CONTRIBUTION(new HashSet<>(Arrays.asList(SituationType.PROPOSED)),
-      new HashSet<>(Arrays.asList(SituationType.REFERENCE, SituationType.NETTING)), new HashSet<>()),
+  PROCESS_CONTRIBUTION(
+      new HashSet<>(Arrays.asList(SituationType.PROPOSED)),
+      new HashSet<>(Arrays.asList(SituationType.REFERENCE, SituationType.NETTING)),
+      new HashSet<>()),
 
   /**
    * At least one temporary scenario (checkboxes)
@@ -46,8 +48,10 @@ public enum CalculationJobType {
    * Optional: 1 Reference
    * NO other scenario types
    */
-  MAX_TEMPORARY_EFFECT(new HashSet<>(Arrays.asList(SituationType.TEMPORARY)),
-      new HashSet<>(Arrays.asList(SituationType.REFERENCE, SituationType.NETTING)), new HashSet<>(Arrays.asList(SituationType.TEMPORARY))),
+  MAX_TEMPORARY_EFFECT(
+      new HashSet<>(Arrays.asList(SituationType.TEMPORARY)),
+      new HashSet<>(Arrays.asList(SituationType.REFERENCE, SituationType.NETTING)),
+      new HashSet<>(Arrays.asList(SituationType.TEMPORARY))),
 
   /**
    * Exactly 1 Project scenario
@@ -56,17 +60,28 @@ public enum CalculationJobType {
    * Optional: one or more ‘In combination_project’ scenarios
    * Optional: one or more ‘in combination reference’ scenarios
    */
-  IN_COMBINATION_PROCESS_CONTRIBUTION(new HashSet<>(Arrays.asList(SituationType.PROPOSED)),
-      new HashSet<>(
-          Arrays.asList(SituationType.REFERENCE, SituationType.NETTING, SituationType.COMBINATION_REFERENCE, SituationType.COMBINATION_PROPOSED)),
+  IN_COMBINATION_PROCESS_CONTRIBUTION(
+      new HashSet<>(Arrays.asList(SituationType.PROPOSED)),
+      new HashSet<>(Arrays.asList(SituationType.REFERENCE, SituationType.NETTING, SituationType.COMBINATION_REFERENCE,
+          SituationType.COMBINATION_PROPOSED)),
       new HashSet<>(Arrays.asList(SituationType.COMBINATION_PROPOSED, SituationType.COMBINATION_REFERENCE))),
 
   /**
    * Only 1 Reference scenario
-   *
    * NO other scenario types
    */
-  DEPOSITION_SUM(new HashSet<>(Arrays.asList(SituationType.REFERENCE)), new HashSet<>(), new HashSet<>());
+  DEPOSITION_SUM(
+      new HashSet<>(Arrays.asList(SituationType.REFERENCE)),
+      new HashSet<>(),
+      new HashSet<>()),
+
+  /**
+   * One scenario (all types allowed)
+   */
+  SINGLE_SCENARIO(
+      new HashSet<>(),
+      new HashSet<>(Arrays.asList(SituationType.values())),
+      new HashSet<>());
 
   private final @JsProperty Set<SituationType> requiredSituationTypes;
   private final @JsProperty Set<SituationType> optionalSituationTypes;
