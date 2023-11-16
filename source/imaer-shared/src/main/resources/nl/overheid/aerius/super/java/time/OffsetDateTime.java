f0bb1246644ec3fabf7ef7c23ca14df70d1d39e4
Bert Scholten <bscholten@dat.nl>
2023-11-16 13:53:38 +0100

    OffsetDateTime GWT object (#229)

---
+++ source/imaer-shared/src/main/resources/nl/overheid/aerius/super/java/time/OffsetDateTime.java
@@ -0,0 +1,7 @@
+// GWT wrapper object for java.time.OffsetDateTime.
+// Needed to use imaer-shared in GWT context as OffsetDateTime is used, although the object using it is not used on the client, but is on the classpath.
+package java.time;
+
+public class OffsetDateTime {
+
+}
