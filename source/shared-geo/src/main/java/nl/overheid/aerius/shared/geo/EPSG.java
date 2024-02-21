37800b089f78b88768f4a791e728a6b0367dfed0
JornC <jorncruijsen@gmail.com>
2024-02-21 13:09:02 +0100

    Get enum by EPSG code

--- source/shared-geo/src/main/java/nl/overheid/aerius/shared/geo/EPSG.java
+++ source/shared-geo/src/main/java/nl/overheid/aerius/shared/geo/EPSG.java
@@ -58,6 +58,16 @@ public enum EPSG {
     return null;
   }
 
+  public static EPSG getEnumByEpsg(final String epsgCode) {
+    try {
+      final int srid = Integer.parseInt(epsgCode.substring(EPSG_PREFIX.length()));
+      return getEnumBySrid(srid);
+    } catch (final NumberFormatException e) {
+      // Eat and return null
+      return null;
+    }
+  }
+
   public static EPSG safeValueOf(final String epsg) {
     try {
       return "".equals(epsg) ? null : valueOf(epsg);
