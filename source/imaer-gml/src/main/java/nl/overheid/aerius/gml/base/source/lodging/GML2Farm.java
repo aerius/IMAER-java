30e125ddc37fee74d12114e15a98ee14276fd03a
Bert Scholten <bscholten@dat.nl>
2024-06-07 14:31:04 +0200

    AER-2858 description when farm lodging could not be converted (#282)
    Instead of an empty description, the description should now contain the main codes used in the old lodging definition. Only the main one, not any of the additional stuff, that would just get messy.

--- source/imaer-gml/src/main/java/nl/overheid/aerius/gml/base/source/lodging/GML2Farm.java
+++ source/imaer-gml/src/main/java/nl/overheid/aerius/gml/base/source/lodging/GML2Farm.java
@@ -16,6 +16,10 @@
  */
 package nl.overheid.aerius.gml.base.source.lodging;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -126,7 +130,10 @@ public class GML2Farm<T extends IsGmlFarmLodgingEmissionSource> extends Abstract
     customEmissions.setAnimalTypeCode(standardLodging.getCode() == null || standardLodging.getCode().length() <= 1
         ? UNKNOWN_ANIMAL_TYPE_CODE
         : standardLodging.getCode().substring(0, 1));
-    customEmissions.setDescription("");
+    final List<String> descriptionParts = new ArrayList<>();
+    Optional.ofNullable(standardLodging.getCode()).ifPresent(descriptionParts::add);
+    Optional.ofNullable(standardLodging.getLodgingSystemDefinitionCode()).ifPresent(descriptionParts::add);
+    customEmissions.setDescription(String.join(", ", descriptionParts));
     customEmissions.setFarmEmissionFactorType(FarmEmissionFactorType.PER_ANIMAL_PER_YEAR);
     customEmissions.getEmissionFactors().put(Substance.NH3, 0.0);
     // Warn the user that this source has been converted to custom animal housing.
